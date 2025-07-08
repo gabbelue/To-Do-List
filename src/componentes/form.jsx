4edda9942fede27756677d9b6418f4b800af1c62
gabbelue <m.gabriely@escolar.ifrn.edu.br>
2025-07-07 22:21:46 -0300

    adição de label para melhor visualização

--- src/componentes/form.jsx
+++ src/componentes/form.jsx
@@ -68,14 +68,18 @@ export default function Form() {
       <Filter tarefas={tarefas} setTarefas={setTarefas} />
 
       <form onSubmit={adicionaTarefa}>
+        <label htmlFor="nome">Nome da tarefa:</label>
         <input
+          id="nome"
           type="text"
           placeholder="Digite o nome da tarefa"
           value={tarefa.nome}
           onChange={(e) => setTarefa({ ...tarefa, nome: e.target.value })}
         />
 
+        <label htmlFor="status">Status:</label>
         <select
+          id="status"
           value={tarefa.status}
           onChange={(e) => setTarefa({ ...tarefa, status: e.target.value })}
         >
@@ -85,11 +89,11 @@ export default function Form() {
           <option value="Realizada">Realizada</option>
         </select>
 
+        <label htmlFor="prioridade">Prioridade:</label>
         <select
+          id="prioridade"
           value={tarefa.prioridade}
-          onChange={(e) =>
-            setTarefa({ ...tarefa, prioridade: e.target.value })
-          }
+          onChange={(e) => setTarefa({ ...tarefa, prioridade: e.target.value })}
         >
           <option value="">Escolha a prioridade</option>
           <option value="Alta">Alta</option>
@@ -97,20 +101,20 @@ export default function Form() {
           <option value="Baixa">Baixa</option>
         </select>
 
+        <label htmlFor="dataInicio">Data de Início:</label>
         <input
+          id="dataInicio"
           type="date"
           value={tarefa.dataInicio}
-          onChange={(e) =>
-            setTarefa({ ...tarefa, dataInicio: e.target.value })
-          }
+          onChange={(e) => setTarefa({ ...tarefa, dataInicio: e.target.value })}
         />
 
+        <label htmlFor="dataTermino">Data de Término:</label>
         <input
+          id="dataTermino"
           type="date"
           value={tarefa.dataTermino}
-          onChange={(e) =>
-            setTarefa({ ...tarefa, dataTermino: e.target.value })
-          }
+          onChange={(e) => setTarefa({ ...tarefa, dataTermino: e.target.value })}
         />
 
         <button type="submit">Adicionar Tarefa</button>
